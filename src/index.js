9174a663f7e3de445ee09158158b142dbfc91e3f
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-01-21 18:40:21 -0300

    add tab buttons & related functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import contactTab from "./contact-tab";
 import homeTab from "./home-tab";
 import menuTab from "./menu-tab.js";
+
+function clearContent() {
+  const content = document.querySelector("div#content");
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+}
+
+const tabButtons = document.querySelectorAll("button.tab");
+tabButtons.forEach((e) => {
+  e.addEventListener("click", () => {
+    clearContent();
+    switch (e.textContent) {
+      case "Home":
+        homeTab();
+        break;
+      case "Menu":
+        menuTab();
+        break;
+      case "Contact":
+        contactTab();
+        break;
+    }
+  });
+});
+
+homeTab();
